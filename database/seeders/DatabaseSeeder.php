2a4c7a37af69b6a88488e91ad9187d4bd3bab31a
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-11-14 17:15:19 -0300

    Fix example env and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
-            SolicitationSeeder::class,
         ]);
     }
 }
