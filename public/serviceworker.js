acbb43905ad8a9aac95775452c71211d993b4320
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-11-17 01:01:20 -0300

    Ajuste cache pwa

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,27 +1,20 @@
-var staticCacheName = "pwa-v" + new Date().getTime();
+var staticCacheName = 'pwa-v' + new Date().getTime();
 var filesToCache = [
+    '/',
     '/offline',
-    '/css/app.css',
-    '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    '/pwa/pwa-192x192.png',
+    '/pwa/pwa-512x512.png',
+    '/pwa/maskable-icon-512x512.png',
 ];
 
 // Cache on install
-self.addEventListener("install", event => {
-    this.skipWaiting();
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
-        caches.open(staticCacheName)
-            .then(cache => {
-                return cache.addAll(filesToCache);
-            })
-    )
+        caches.open(staticCacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        }).catch(() => Promise.resolve())
+    );
 });
 
 // Clear cache on activate
